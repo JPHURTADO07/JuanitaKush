f74e533a12766da198bd56ac83f80bcacc5bf71a
Juan Pablo Orrego Hurtado <44008914+JPHURTADO07@users.noreply.github.com>
2026-08-20 14:30:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Conversión de Texto a Audio")
+st.title("En este lugar te enseñamos a cocinar de la mano de tu agente de IA")
 image = Image.open('IA.jpg')
 st.image(image, width=350)
 with st.sidebar:
-    st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
+    st.subheader("Agente de IA")
 
 
 try:
@@ -19,17 +19,10 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
-        
-        )
+st.subheader("El Sabor del Futuro: Cocina Inteligente a tu Alcance")
+st.write('La cocina ya no es solo cuestión de intuición; ahora tienes un copiloto digital listo para ayudarte a crear platos extraordinarios.')
            
-st.markdown(f"Quieres escucharlo?, copia el texto")
+st.markdown(f"¿Listo para llevar tu cocina al siguiente nivel? Es el momento de romper el hielo y empezar a charlar con tu agente de IA")
 text = st.text_area("Ingrese El texto a escuchar.")
 
 tld='com'
