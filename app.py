3c73e2454d0f6ad9edcd4c6084c3819914352bf3
Juan Pablo Orrego Hurtado <44008914+JPHURTADO07@users.noreply.github.com>
2026-08-20 14:14:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+st.title("En este lugar te enseñamos a cocinar de la mano de tu agente de IA.")
+image = Image.open('IA.png')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
